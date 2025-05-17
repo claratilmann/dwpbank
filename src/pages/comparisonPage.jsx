f215f6fd9cd6c5cd69a5dd3aae94601b83fc6a0c
Clara Henrike Tilmann <clara.tilmann@gmail.com>
2025-05-17 15:54:31 +0200

    add table

--- src/pages/comparisonPage.jsx
+++ src/pages/comparisonPage.jsx
@@ -1,4 +1,55 @@
-import React, { useState } from "react";
-const ComparisonPage = () => {};
+import React from "react";
+import {
+  Table,
+  TableHead,
+  TableBody,
+  TableRow,
+  TableCell,
+  TableHeader,
+  Button,
+} from "@carbon/react";
+import securities from "../data.json";
+
+const headers = [
+  "wkn",
+  "isin",
+  "Name",
+  "Typ",
+  "Kurs",
+  "Anlagerisiko",
+  "Hauptversammlung",
+  "Emittent",
+  "Aktion",
+];
+
+const ComparisonPage = () => (
+  <div>
+    <Table size="lg" useZebraStyles={false}>
+      <TableHead>
+        <TableRow>
+          {headers.map((header) => (
+            <TableHeader id={header.key} key={header}>
+              {header}
+            </TableHeader>
+          ))}
+        </TableRow>
+      </TableHead>
+      <TableBody>
+        {securities.map((row) => (
+          <TableRow key={row.wkn}>
+            {Object.keys(row)
+              .filter((key) => key !== "id")
+              .map((key) => {
+                return <TableCell key={key}>{row[key]}</TableCell>;
+              })}
+            <TableCell>
+              <Button>Entfernen</Button>
+            </TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
+  </div>
+);
 
 export default ComparisonPage;
